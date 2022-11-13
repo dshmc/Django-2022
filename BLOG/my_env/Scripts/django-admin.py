4b68d77aedf40dfc6b29efb036d4fd861d123e13
Andrii Marynets <42659281+dshmc@users.noreply.github.com>
2022-11-12 23:57:56 -0500

    clear

--- BLOG/my_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\dshmc95\desktop\django-2.1-\blog\my_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- README.md
+++
@@ -1,3 +0,0 @@
-# Django-2.1-
-Practice Building a Website in Python
-Start 30.08.2020
