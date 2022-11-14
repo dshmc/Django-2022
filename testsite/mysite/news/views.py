bd23a4d091cb13f4c119b4b57f9ef41620138edd
Andrii Marynets <42659281+dshmc@users.noreply.github.com>
2022-11-13 21:25:54 -0500

    controllers and routes

--- testsite/mysite/news/views.py
+++ testsite/mysite/news/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    print(request)
+    return HttpResponse('<h1>Hello world</h1>')
+
+def test(request):
+    return HttpResponse('<h1> Тестова сторінка </h1>')
