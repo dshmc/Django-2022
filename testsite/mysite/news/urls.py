c5fd696307130454e722560a23ca60aae8e1ad2b
Andrii Marynets <42659281+dshmc@users.noreply.github.com>
2022-11-14 22:03:28 -0500

    test_!

--- testsite/mysite/news/urls.py
+++ testsite/mysite/news/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', index),
     path('test/', test),
+    path('test_1/', first_test),
 ]
--- testsite/mysite/news/views.py
+++ testsite/mysite/news/views.py
@@ -1,3 +1,4 @@
+#from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
@@ -6,3 +7,6 @@ def index(request):
 
 def test(request):
     return HttpResponse('<h1> Тестова сторінка </h1>')
+
+def first_test(request):
+    return HttpResponse('<h3>Я майбутній Джанго розробник.</h33>')
